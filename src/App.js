5f888093268df37763923c0cbe1a7e0a174cad7d
Kristyna Marvalova <k.marvalova@outlook.com>
2022-01-25 12:08:29 +0100

    mostly finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Card from './components/Card';
+import Header from './components/Header'
+
+import Data from './Data'
+
 function App() {
+  const card = Data.map (item => {
+    return <Card {...item}/>
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header fontawesome='fas fa-campground' title='Travel Journal'/>
+      <div className='page'>
+      {card}
+      </div>
+    </>
+    
   );
 }
 
